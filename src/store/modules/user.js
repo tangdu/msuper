c688f844925db644f82ba84247db5ad694c10949
唐杜 <tangdu0228@@aliyun.com>
2018-01-22 16:49:53 +0800

    修改目录

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,16 +1,39 @@
-import { userMutations } from "../mutations/user";
-import { userActions } from "../actions/user";
+import * as types from '../constants/types';
+import axios from 'axios';
 
-const user = {
-  state: {
+const state = {
     isFetching: false,
     data: {},
     userInfo: {
-      loginname: "",
-      score: 0
+        loginname: '',
+        score: 0
     }
-  },
-  mutations: userMutations,
-  actions: userActions
 };
-export default user;
+const getters = {};
+const actions = {
+    /**
+     *
+     * @param {accesstoken} [String]
+     * @param {isRefresh} [Boolean]
+     */
+    fetchUserAction({ commit, state, dispatch }, params) {
+        state.isFetching = true;
+        commit(types.FETCH_USER_SUC, {
+            data: { loginname: '唐杜', score: 100 }
+        });
+    }
+};
+
+const mutations = {
+    [types.FETCH_USER_SUC](state, action) {
+        state.userInfo = action.data;
+        state.isFetching = false;
+    }
+};
+
+export default {
+  state,
+  getters,
+  actions,
+  mutations
+};
--- src/store/mutations/user.js
+++
@@ -1,8 +0,0 @@
-import * as types from '../constants/types'
-
-export const userMutations = {
-  [types.FETCH_USER_SUC](state, action) {
-    state.userInfo = action.data
-    state.isFetching = false;
-  }
-}
